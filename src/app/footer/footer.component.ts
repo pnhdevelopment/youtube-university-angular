e8ea9212ac79aed8bd194c8185f5a6bfe1905fa9
Pascale <pnhdevelopment@gmail.com>
2019-04-09 15:42:26 +1000

    Edited footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.sass']
 })
 export class FooterComponent implements OnInit {
-
+  
+  objDate = Date.now();
+  
   constructor() { }
 
   ngOnInit() {
